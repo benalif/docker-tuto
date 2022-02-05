8a7520b95b440ee201d97c104c46c2ef8cf6ceff
Farid BENALI <benalif@wataniya-algerie.com>
2022-02-05 13:46:19 +0100

    first commit

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,14 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import { render, screen } from "@testing-library/react";
+import App from "./App";
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+describe("Login", () => {
+  test("renders learn react link", () => {
+    render(<App />);
+    const linkElement = screen.getByText(/learn react/i);
+    expect(linkElement).toBeInTheDocument();
+  });
+
+  test("function should return addition", () => {
+    expect(2 + 2).not.toBe(5);
+  });
 });
